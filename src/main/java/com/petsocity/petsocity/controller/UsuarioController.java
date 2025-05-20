5002482fc6234ea1255d8821efd94865a267e071
Pipeto <fel.navarroc@hotmail.com>
2025-05-20 15:47:22 -0400

    Se crean nuevas clases y cominzo a tragbjar en controllador

--- src/main/java/com/petsocity/petsocity/controller/UsuarioController.java
+++ src/main/java/com/petsocity/petsocity/controller/UsuarioController.java
@@ -1,5 +1,15 @@
 package com.petsocity.petsocity.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.petsocity.petsocity.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController
 
 }
