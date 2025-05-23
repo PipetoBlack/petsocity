6ab06c60ae51e5707361f74eab7a9a40a54c684d
MobbPanda <al.astudilloh@duocuc.cl>
2025-05-23 15:01:09 -0400

    Se añade contenido en carritoRepository

--- src/main/java/com/petsocity/petsocity/repository/CarritoRepository.java
+++ src/main/java/com/petsocity/petsocity/repository/CarritoRepository.java
@@ -1,9 +1,19 @@
 package com.petsocity.petsocity.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.petsocity.petsocity.model.Carrito;
+import com.petsocity.petsocity.model.EstadoCarrito;
 
 public interface CarritoRepository extends JpaRepository<Carrito,Long>{
+    // Carritos asociados al usuario
+    List<Carrito> findByUsuarioId(Long usuarioId);
+
+    // Trae los carritos de acuerdo al estado
+    List<Carrito> findByEstado(EstadoCarrito estado);
+
+    // Trae los carritos de acuerdo al id del usuario, y al estado
+    List<Carrito> findByUsuarioIdAndEstado(Long usuarioId, EstadoCarrito estado);
 
 }
