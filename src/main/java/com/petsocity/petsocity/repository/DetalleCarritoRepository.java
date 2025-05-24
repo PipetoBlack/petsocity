70df507c217031b35693bbd5a7039066c6e7919b
r4coo <racoracobrez@gmail.com>
2025-05-24 00:45:09 -0400

    cambio detalle

--- src/main/java/com/petsocity/petsocity/repository/DetalleCarritoRepository.java
+++ src/main/java/com/petsocity/petsocity/repository/DetalleCarritoRepository.java
@@ -1,9 +1,21 @@
 package com.petsocity.petsocity.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.petsocity.petsocity.model.DetalleCarrito;
 
 public interface DetalleCarritoRepository extends JpaRepository<DetalleCarrito,Long>{
+        //Traer por el id del usuario
+        List<DetalleCarrito>findByUsuarioId(long usuarioid);
 
+            // Obtener todos los productos de un carrito
+        List<DetalleCarrito> findByCarritoId(Long carritoId);
+
+        // Buscar todos los carritos que contienen un producto específico
+        List<DetalleCarrito> findByInventarioId(Long inventarioId);
+
+        // Buscar si un carrito ya contiene cierto producto
+        DetalleCarrito findByCarritoIdAndInventarioId(Long carritoId, Long inventarioId);
 }
