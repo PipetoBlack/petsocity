a635b3e17736334a70a30a5bf285b7aa2eec24e7
Pipeto <fel.navarroc@hotmail.com>
2025-06-07 10:11:19 -0400

    limpieza

--- usUsuario/src/main/java/com/petsocity/petsocity/DataLoader.java
+++ usUsuario/src/main/java/com/petsocity/petsocity/DataLoader.java
@@ -1,10 +1,7 @@
 package com.petsocity.petsocity;
 
 import net.datafaker.Faker;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
