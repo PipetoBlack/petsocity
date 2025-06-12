cfe6df8552b27636084f9677d3ba0f7aa2574193
MobbPanda <al.astudilloh@duocuc.cl>
2025-06-11 21:49:55 -0400

    Se realiza Test en usuario y carrito

--- usCarrito/src/test/java/com/petsocity/petsocity/PetsocityApplicationTests.java
+++ usCarrito/src/test/java/com/petsocity/petsocity/PetsocityApplicationTests.java
@@ -2,9 +2,18 @@ package com.petsocity.petsocity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PetsocityApplicationTests {
+
+	@Autowired
+	private CarritoController usuarioController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
--- usUsuario/src/test/java/com/petsocity/petsocity/PetsocityApplicationTests.java
+++ usUsuario/src/test/java/com/petsocity/petsocity/PetsocityApplicationTests.java
@@ -2,12 +2,40 @@ package com.petsocity.petsocity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import com.petsocity.petsocity.controller.UsuarioController;
 
 @SpringBootTest
 class PetsocityApplicationTests {
 
+	@Autowired
+	private UsuarioController usuarioController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
 	@Test
 	void contextLoads() {
+        System.out.println("Testing the context loading...");
+        // System.out.println("Server running on port: " + port);
 	}
+
+    @Test
+    void contextLoads2() throws Exception {
+        System.out.println("Testing the context loading. and the controller...");
+        assertThat(usuarioController).isNotNull();
+    }
+
+    @Test
+    void getLibrosContainsBrackets() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port +
+                "/api/v1/libros",
+                String.class)).toString().contains("[");
+    }
 
 }
