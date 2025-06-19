1467641aef46ba5da2e3c5d9b6f717dfaea8ef39
Pipeto <fel.navarroc@hotmail.com>
2025-06-18 21:53:38 -0400

    Se crea carpeta config y se añade la clase SwaggerConfig

--- usUsuario/src/main/java/com/petsocity/petsocity/config/SwaggerConfig.java
+++ usUsuario/src/main/java/com/petsocity/petsocity/config/SwaggerConfig.java
@@ -1,5 +1,19 @@
 package com.petsocity.petsocity.config;
 
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+
+@Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info()
+            .title("API 2025 Usuarios")
+            .version("1.0")
+            .description("Documentacion de la API para el sistema de usuarios"));
+    }
 }
