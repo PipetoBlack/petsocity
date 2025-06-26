32d54778c54ca45703366492d4a0176c137f8493
MobbPanda <al.astudilloh@duocuc.cl>
2025-06-26 18:28:19 -0400

    Se agregan clases para Testing

--- usInventario/src/test/java/com/petsocity/petsocity/PetsocityApplicationTests.java
+++ usInventario/src/test/java/com/petsocity/petsocity/PetsocityApplicationTests.java
@@ -1,7 +1,25 @@
 package com.petsocity.petsocity;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.petsocity.petsocity.controller.InventarioController;
+import com.petsocity.petsocity.model.Inventario;
+
+import net.datafaker.Faker;
+
+// @SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 
 @SpringBootTest
 class PetsocityApplicationTests {
